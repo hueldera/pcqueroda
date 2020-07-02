8b58ed9fbd241ece143174b86c10343365532c62
Huelder <huelderantuness@gmail.com>
2020-07-02 13:34:05 -0300

    change: default searcher route

--- pcqueroda/urls.py
+++ pcqueroda/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('searcher/', include('searcher.urls')),
+    path('', include('searcher.urls')),
     path('admin/', admin.site.urls),
 ]
