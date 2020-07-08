6f70dbda0fd0dff7ca776bf01c79e24538085229
Huelder <huelderantuness@gmail.com>
2020-07-08 18:53:08 -0300

    change: new configs and layout

--- searcher/models.py
+++ searcher/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 GAME_CATEGORIES = [('A', 'A'), ('AA', 'AA'), ('AAA', 'AAA')]
 ITEM_TYPES = [('Game', 'Game'), ('Software', 'Software'), ('Other', 'Other')]
-RAM_LIST = [(1,'1GB'), (2, '2GB'), (3, '4GB'), (4, '6GB'), (5, '8GB'), 
+RAM_LIST = [(1,'1GB'), (2, '2GB'), (3, '4GB'), (4, '6GB'), (5, '8GB'),
 (6, '12GB'), (7, '14GB'), (8, '16GB'), (9, '32GB'), (10, '64GB+')]
-PROCESSOR_LIST = [(1, 'Lower Pentium/Athlon'), (2, 'Higher Pentium/Athlon'), 
-(3, 'Lower i3/Ryzen/FX' ), (4, 'Higher i3/Ryzen/FX' ), (5, 'Lower i5/Ryzen/FX'), 
+PROCESSOR_LIST = [(1, 'Lower Pentium/Athlon'), (2, 'Higher Pentium/Athlon'),
+(3, 'Lower i3/Ryzen/FX' ), (4, 'Higher i3/Ryzen/FX' ), (5, 'Lower i5/Ryzen/FX'),
 (6, 'Higher i5/Ryzen/FX' ), (7, 'Lower i7/Ryzen/FX'), (8, 'Higher i7/Ryzen/FX'),
 (9 ,'Lower i9/Ryzen' ), (10, 'Higher i9/Ryzen' )]
 GRAPHICS_LIST = [
-(1, 'Lower Intel/GT/Vega'), (2, 'Medium Intel/GT/Vega'), (3, 'Higher Intel/GT/Vega'), 
-(4, 'Lower Intel/GTX/Radeon'), (5, 'Intel/GTX/Radeon'), (6, 'Higher GTX/Radeon' ), 
-(5, 'Lower RTX/Radeon'), (6, 'Medium RTX/Radeon'), (7, 'Higher RTX/Radeon' ), 
+(1, 'Lower Intel/GT/Vega'), (2, 'Medium Intel/GT/Vega'), (3, 'Higher Intel/GT/Vega'),
+(4, 'Lower Intel/GTX/Radeon'), (5, 'Intel/GTX/Radeon'), (6, 'Higher GTX/Radeon' ),
+(5, 'Lower RTX/Radeon'), (6, 'Medium RTX/Radeon'), (7, 'Higher RTX/Radeon' ),
 (8, 'Lower Titan/Radeon'), (9, 'Medium Titan/Radeon'), (10, 'Higher Titan/Radeon')
 ]
 
