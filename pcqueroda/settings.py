5835148766ad686f3b92115f41235e8b93f911be
Huelder <huelderantuness@gmail.com>
2020-07-21 18:25:00 -0300

    add: django_heroku

--- pcqueroda/settings.py
+++ pcqueroda/settings.py
@@ -1,7 +1,9 @@
 import os
 import dynaconf
+import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 settings = dynaconf.DjangoDynaconf(__name__)  # noqa
 
+django_heroku.settings(locals())
